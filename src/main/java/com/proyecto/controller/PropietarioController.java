1ee666ab44073883eafb6a6a0dd0981bc6360c50
AriasJuarezAlexandro <ariasjuarezalex@gmail.com>
2022-04-19 16:44:08 -0500

    controller pro

--- src/main/java/com/proyecto/controller/PropietarioController.java
+++ src/main/java/com/proyecto/controller/PropietarioController.java
@@ -1,5 +1,54 @@
 package com.proyecto.controller;
 
-public class PropietarioController {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.proyecto.entity.Propietario;
+import com.proyecto.service.PropietarioService;
+
+@RestController
+@RequestMapping("/rest/propietario")
+@CrossOrigin(origins = "http://localhost:4200")
+public class PropietarioController {
+	
+	@Autowired
+	private PropietarioService service;
+	
+	@GetMapping
+	@ResponseBody
+	public ResponseEntity<List<Propietario>> listaPropietario(){
+		List<Propietario> lista = service.listaPropietario();
+		return ResponseEntity.ok(lista);
+	}
+	
+	@PostMapping
+	@ResponseBody
+	public  ResponseEntity<Map<String, Object>> insertaModalidad(@RequestBody Propietario obj){
+		Map<String, Object> salida = new HashMap<>();
+		try {
+			Propietario objSalida = service.insertaActualizaPropietario(obj);
+			if (objSalida == null) {
+				salida.put("mensaje", "No se registró, consulte con el administrador.");
+			}else {
+				salida.put("mensaje", "Se registró correctamente.");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			salida.put("mensaje", "No se registró, consulte con el administrador.");
+		}
+		return ResponseEntity.ok(salida);
+	}
+	
+	
 }
