2ed8dec1c40f6e509aad3a101920c6cbfc131b48
AriasJuarezAlexandro <ariasjuarezalex@gmail.com>
2022-04-19 16:29:56 -0500

    Camb

--- src/main/java/com/proyecto/repository/DepartamentoRepository.java
+++ src/main/java/com/proyecto/repository/DepartamentoRepository.java
@@ -1,9 +1,6 @@
 package com.proyecto.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.proyecto.entity.Departamento;
-
-public interface DepartamentoRepository extends JpaRepository<Departamento, Integer>{
+public interface DepartamentoRepository {
 
 }
