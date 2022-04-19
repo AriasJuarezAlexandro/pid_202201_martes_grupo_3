68201899ccd27781b954a923b6b5388bf89184a8
portilloJair <portillojair@gmail.com>
2022-04-19 17:08:51 -0500

    Util

--- src/main/java/com/proyecto/controller/UtilController.java
+++ src/main/java/com/proyecto/controller/UtilController.java
@@ -10,6 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.proyecto.entity.EdificioDepartamento;
+import com.proyecto.entity.EstadoDepartamento;
+import com.proyecto.entity.TipoDepartamento;
+import com.proyecto.service.EdificioDepartamentoService;
+import com.proyecto.service.EstadoDepartamentoService;
+import com.proyecto.service.TipoDepartamentoService;
 import com.proyecto.util.AppSettings;
 
 @RestController
@@ -17,6 +23,29 @@ import com.proyecto.util.AppSettings;
 @CrossOrigin(origins = AppSettings.URL_CROSS_ORIGIN)
 public class UtilController {
 
+	@Autowired
+	private EdificioDepartamentoService edificioDepartamentoService;
+	@Autowired
+	private EstadoDepartamentoService estadoDepartamentoService;
+	@Autowired
+	private TipoDepartamentoService tipoDepartamentoService;
 	
+	@GetMapping("/listaEdificioDepartamento")
+	@ResponseBody
+	public List<EdificioDepartamento> listaEdificioDepartamento(){
+		return edificioDepartamentoService.listaEdificioDepartamento();
+	}
+	
+	@GetMapping("/listaEstadoDepartamento")
+	@ResponseBody
+	public List<EstadoDepartamento> listaEstadoDepartamento(){
+		return estadoDepartamentoService.listaEstadoDepartamento();
+	}
+	
+	@GetMapping("/listaTipoDepartamento")
+	@ResponseBody
+	public List<TipoDepartamento> listaTipoDepartamento(){
+		return tipoDepartamentoService.listaTipoDepartamento();
+	}
 
 }
