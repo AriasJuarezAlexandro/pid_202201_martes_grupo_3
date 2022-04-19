7ae5f611af9cf7bf35ca6f553c082fa6cd7983b1
portilloJair <portillojair@gmail.com>
2022-04-19 16:59:51 -0500

    Servicios

--- src/main/java/com/proyecto/service/DepartamentoService.java
+++ src/main/java/com/proyecto/service/DepartamentoService.java
@@ -1,5 +1,11 @@
 package com.proyecto.service;
 
+import java.util.List;
+
+import com.proyecto.entity.Departamento;
+
 public interface DepartamentoService {
 
+	public abstract List<Departamento> listaDepartamento();
+	public abstract Departamento registrarDepartamento(Departamento departamento);
 }
---
+++ src/main/java/com/proyecto/service/TipoDepartamentoService.java
@@ -0,0 +1,10 @@
+package com.proyecto.service;
+
+import java.util.List;
+
+import com.proyecto.entity.TipoDepartamento;
+
+public interface TipoDepartamentoService {
+
+	public abstract List<TipoDepartamento> listaTipoDepartamento();
+}
