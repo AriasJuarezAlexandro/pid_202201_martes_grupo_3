6629533aae0dc9f9c320639e857ee89c99689c4a
AloeStyleZ <51101232+AloeStyleZ@users.noreply.github.com>
2022-04-25 17:35:35 -0500

    repositorio Visitante

---
+++ src/main/java/com/proyecto/repository/VisitanteRepository.java
@@ -0,0 +1,9 @@
+package com.proyecto.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.proyecto.entity.Visitante;
+
+public interface VisitanteRepository extends JpaRepository<Visitante, Integer>{
+
+}
