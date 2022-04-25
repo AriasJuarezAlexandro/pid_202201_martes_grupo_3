a635aa5596c4c0794af7ba33975d1a208ad80bcf
AloeStyleZ <51101232+AloeStyleZ@users.noreply.github.com>
2022-04-25 04:43:19 -0500

    entidad Visitante

---
+++ src/main/java/com/proyecto/entity/Visitante.java
@@ -0,0 +1,30 @@
+package com.proyecto.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "TB_VISITANTE")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Visitante {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idVis;
+	private String nomVis;
+	private String apeVis;
+	
+	
+	/*soon*/
+	
+}
