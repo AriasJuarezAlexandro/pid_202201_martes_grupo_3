98c42c60b215fcc6d2725442d2a3e0ef29e95f3f
jp150 <jp150@192.168.1.5>
2022-05-01 17:15:35 -0500

    login

---
+++ src/main/java/com/proyecto/security/SecurityConfig.java
@@ -0,0 +1,67 @@
+package com.proyecto.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.proyecto.service.AdministradorService;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	private AdministradorService userDetails;
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+		auth.setUserDetailsService(userDetails);
+		auth.setPasswordEncoder(passwordEncoder());
+		return auth;
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(authenticationProvider());
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+		.authorizeRequests().antMatchers(
+				"/registro**",
+				"/js/**",
+				"/css/**",
+				"/img/**").permitAll()
+		.and()
+		.authorizeRequests()
+		.antMatchers("/administrador/*").authenticated()
+		.and().formLogin()
+		.loginPage("/login")
+		.defaultSuccessUrl("/administrador/menu",true)
+		.usernameParameter("username")
+		.passwordParameter("password")
+		.and()
+		.logout()
+		.invalidateHttpSession(true)
+		.clearAuthentication(true)
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+		.logoutSuccessUrl("/login?logout")
+		.permitAll();;
+	}
+}
+
---
+++ src/main/java/com/proyecto/security/WebSecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.proyecto.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class WebSecurityInitializer extends AbstractSecurityWebApplicationInitializer{
+
+}
