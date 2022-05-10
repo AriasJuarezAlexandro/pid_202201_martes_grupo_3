a2004140af6e8b9f1dd379a84956c7218c5807a7
AloeS <AloeS@DESKTOP-S34MA7P>
2022-05-10 14:48:24 -0500

    Controller & completado de servicios Ocupante

--- src/main/java/com/proyecto/service/OcupanteService.java
+++ src/main/java/com/proyecto/service/OcupanteService.java
@@ -1,5 +1,10 @@
 package com.proyecto.service;
 
-public interface OcupanteService {
+import java.util.List;
 
+import com.proyecto.entity.Ocupante;
+
+public interface OcupanteService {
+	public abstract Ocupante registrarOcupante(Ocupante ocupante);
+	public abstract List<Ocupante> listarOcupantes();
 }
--- src/main/java/com/proyecto/service/OcupanteServiceImpl.java
+++ src/main/java/com/proyecto/service/OcupanteServiceImpl.java
@@ -1,5 +1,26 @@
 package com.proyecto.service;
 
-public class OcupanteServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.proyecto.entity.Ocupante;
+import com.proyecto.repository.OcupanteRepository;
+
+@Service
+public class OcupanteServiceImpl implements OcupanteService{
+	@Autowired
+	private OcupanteRepository repository;
+	
+	@Override
+	public Ocupante registrarOcupante(Ocupante ocupante) {
+		return repository.save(ocupante);
+	}
+
+	@Override
+	public List<Ocupante> listarOcupantes() {
+		return repository.findAll();
+	}
 
 }
